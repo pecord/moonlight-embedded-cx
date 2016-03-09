eac25310119fd426f251ea7e68c9659a9c21eacb
OtherCrashOverride <OtherCrashOverride@github.com>
2016-03-09 05:04:15 -0500

    Update copyright.

--- src/video/odroidcx.c
+++ src/video/odroidcx.c
@@ -2,6 +2,7 @@
  * This file is part of Moonlight Embedded.
  *
  * Copyright (C) 2015 Iwan Timmer
+ * Copyrgiht (C) 2016 OtherCrashOverride
  *
  * Moonlight is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
